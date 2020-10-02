a360332e2efdc196a91713c5dfe8bd6f83c0a56c
TheusVask <matheus_vasconelos98@hotmail.com>
2020-10-02 15:41:54 -0300

    Init operations at JS

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 const valueDisplay = document.getElementById('value');
+const digitos = document.getElementById('digitos');
 
-const teste = document.getElementById('digitos');
+const equal = document.getElementsByClassName('equal')[0];
 
-teste.addEventListener('click', (e)=>{calcula(e.toElement.innerText)})
+digitos.addEventListener('click', (e)=>{validaNum1(e.toElement.innerText)});
 
-function calcula(val){
-  if(Number(val) || val == 0){
-    console.log(val)
-  }else{console.log('Não é numero')}
+equal.addEventListener('click', ()=>{console.log(calcula())})
+
+let valores = {
+  num1: 0,
+  operator: '',
+  num2: 0
 }
 
+function validaNum1(val){
+  if(Number(val) || val == 0){
+      recebeNum(val)
+  }else if(val != '=' && val != 'C'){
+    valores.operator = val;
+    console.log(valores.operator)
+  }
+}
+
+function recebeNum(num){
+  if(valores.operator == ''){
+    const atual = valores.num1;
+    const post = num;
+    valores.num1 = Number(`${valores.num1}${num}`)
+    console.log(valores.num1)
+  }else{
+    const atual = valores.num2;
+    const post = num;
+    valores.num2 = Number(`${valores.num2}${num}`)
+    console.log(valores.num2)
+  }
+}
+
+
+function calcula(){
+  if(valores.operator == '/'){
+    return valores.num1 / valores.num2
+  }
+}
